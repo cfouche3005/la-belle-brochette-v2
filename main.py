7c33a364ac496d56e398ebf9d99d2b94a18fe8da
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-04 09:22:13 +0200

    Add environment and camera initialization to game setup

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from game.runtime import Runtime
 from entities.player.player import Player
+from game.camera import Camera
+from game.env import Env
 
 if __name__ == '__main__':
     # Initialize the game
     game = Runtime((1280, 720))
     # Create a player instance
+
+    env = Env(1280, 720, "assets/bg.jpeg")
+    camera = Camera(1280, 720)
     player = Player(100, 100, 50, 50)
-    game.setup(player)
+
+    game.setup(player, env, camera)
     game.run()
