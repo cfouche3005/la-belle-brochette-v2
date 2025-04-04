afc7712baee00cf5b946350e90eea2457a6d3f84
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-04 09:22:31 +0200

    Refactor Player update method to incorporate environment and camera for collision detection

--- entities/player/player.py
+++ entities/player/player.py
@@ -1,12 +1,17 @@
 import pygame
 
+from game.camera import Camera
+from game.env import Env
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         super().__init__()
         self.image = pygame.Surface((width, height))
         self.image.fill((255, 44, 102))
         self.rect = self.image.get_rect()
-
+        self.width = width
+        self.height = height
         self.rect.x = x
         self.rect.y = y
         self.speed = 5
@@ -14,12 +19,13 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.5
         self.velocity = 0
 
-    def update(self):
+    def update(self, env: Env, camera: Camera):
         keys = pygame.key.get_pressed()
+        tempX = self.rect.x
         if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
+            tempX-= self.speed
         if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+            tempX += self.speed
         if keys[pygame.K_UP]:
             self.jump()
 
@@ -34,6 +40,19 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y < 0:
             self.rect.y = 0
             self.velocity = 0
+        # Check for collision with the left wall
+
+        if tempX < env.invisibleWidth:
+            if(env.moveLeft()):
+                tempX = self.rect.x
+        if tempX< 0:
+            tempX = 0
+        if tempX + self.width > env.screenWidth-env.invisibleWidth:
+            if(env.moveRight()):
+                tempX = self.rect.x
+        if tempX + self.width > camera.width:
+            tempX = camera.width - self.width
+        self.rect.x = tempX
 
     def jump(self):
         if self.rect.y == 500:
