66ba8a445fd0ed75a1310c481ebeec2b432c1eb6
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-09 13:45:43 +0200

    Enhance Env class to support dynamic platform and element loading with textures

--- game/env.py
+++ game/env.py
@@ -1,7 +1,25 @@
+import random
+
 import pygame
 
+from environnement.environnement_jeu import plateformes_fixes, Plateforme, elements_sol_fixes, ElementAuSol
+from game.camera import Camera
+
+PLATFORME_TEXTUREPATH = {
+    "assets1": "assets/VOITURE2.png",
+    "assets2": "assets/GROUND.jpg",
+}
+
+ELEMENT_TEXTUREPATH = {
+    "porte": "assets/PORTE1.png",
+    "escalier": "assets/ESCALIER1.png",
+    "trou": "assets/TROU1.png",
+    "crayon": "assets/CRAYON_JW.png"
+
+}
+
 class Env:
-    def __init__(self, width : int, height : int, background : str):
+    def __init__(self, width : int, height : int, background : str, screenInstance : pygame.Surface, camera: Camera):
         self.screenWidth = width
         self.invisibleWidth = 200
         self.screenHeight = height
@@ -12,7 +30,17 @@ class Env:
         self.background_path = background
         self.background = None
 
+        self.screen = screenInstance
+        self.camera = camera
+
+        #Initialisation des groupes
+        self.platforms = pygame.sprite.Group()
+        self.element_group = pygame.sprite.Group()
+
+
         self.loadbackground()
+        self.loadPlatforms()
+        self.loadElements()
 
     def loadbackground(self):
         try:
@@ -23,8 +51,25 @@ class Env:
 
         except Exception as e:
             print(f"Error loading background: {e}")
-    def draw(self, surface):
-        surface.blit(self.background, (self.x, self.y))
+
+    def loadPlatforms(self):
+        for x, y, width, height in plateformes_fixes:
+            # Choix aléatoire de la texture parmi les options disponibles
+            texture = random.choice(list(PLATFORME_TEXTUREPATH.values()))
+            platform = Plateforme(x, y, width, height, texture)
+            self.platforms.add(platform)
+    def loadElements(self):
+        for x, y, type_element in elements_sol_fixes:
+            if type_element in ELEMENT_TEXTUREPATH:
+                element = ElementAuSol(x, y, 50, 50, ELEMENT_TEXTUREPATH[type_element])
+                self.element_group.add(element)
+    def draw(self):
+        bg_rect = pygame.Rect(self.x + self.camera.offset_x, self.y, self.width, self.height)
+        self.screen.blit(self.background, bg_rect)
+        for platform in self.platforms:
+            self.screen.blit(platform.image, self.camera.apply(platform))
+        for element in self.element_group:
+            self.screen.blit(element.image, self.camera.apply(element))
 
     def moveRight(self):
         self.x -= 5
