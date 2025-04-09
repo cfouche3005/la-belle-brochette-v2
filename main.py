c5dbb0109aca21380f5c7f99d6b66802c32aa068
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-09 13:45:51 +0200

    Update player initialization and simplify game setup in main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
 
     game = Runtime((1280, 690))
 
-    env = Env(1280, 720, "assets/bg.jpeg")
     camera = Camera(1280, 720, 1280*2)
-    player = Player(10, 535, 50, 50)
+    player = Player(100, 150, 50, 50)
     pu_group = pygame.sprite.Group()
 
-    game.setup(player, env, camera, game.power_ups, game.platforms, game.element_group)
+    game.setup(player, camera)
     game.run()
 
