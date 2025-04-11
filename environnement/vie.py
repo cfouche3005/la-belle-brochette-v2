6552ca450d926856c011ccdef6b747446af900e3
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-11 02:19:10 +0200

    Add game over callback to BarreDeVie class for improved game state handling

--- environnement/vie.py
+++ environnement/vie.py
@@ -1,13 +1,15 @@
 import pygame
 
 class BarreDeVie:
-    def __init__(self, max_vies=5):
+    def __init__(self, max_vies=5, game_over_cb=None):
         self.vies = max_vies
         self.max_vies = max_vies
         self.x = 20
         self.y = 20
         self.coeur_image = pygame.image.load("assets/COEUR_PA.png").convert_alpha()
         self.coeur_image = pygame.transform.scale(self.coeur_image, (50, 50))
+        print("Game over callback:", game_over_cb)
+        self.gameOverCB = game_over_cb
 
     def perdre_vie(self, damage):
         """
@@ -16,6 +18,9 @@ class BarreDeVie:
         self.vies -= damage
         if self.vies < 0:
             self.vies = 0
+            if self.gameOverCB:
+                print("Game Over")
+                self.gameOverCB()
 
     def draw(self, screen):
         for i in range(self.vies):
