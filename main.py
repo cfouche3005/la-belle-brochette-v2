39a48e8f96d3eb5743d7f4015eaad8b7d18a3d49
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2025-04-11 02:18:35 +0200

    Refactor main function for improved code structure and readability

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from game.camera import Camera
 from game.env import Env
 import pygame
 
-if __name__ == '__main__':
-
+def main():
     game = Runtime((1280, 690))
-
-    camera = Camera(1280, 720, 1280*2)
-    player = Player(100, 150, 50, 50)
-
-    game.setup(player, camera)
     game.run()
 
+
+if __name__ == '__main__':
+    main()
